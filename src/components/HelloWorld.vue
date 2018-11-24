570946f95e96189863fe44d01182607060b2160b
unknown <shellteo@163.com>
2018-11-24 19:45:20 +0800

    update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
